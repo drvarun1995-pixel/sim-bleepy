ce1849e9a516c17213a3db26edfe96062d54bc9b
drvarun1995-pixel <drvarun1995@gmail.com>
2025-10-22 18:59:26 +0100

    Fix userId variable error in certificate generate API

--- app/api/certificates/generate/route.ts
+++ app/api/certificates/generate/route.ts
@@ -183,7 +183,7 @@ export async function POST(request: NextRequest) {
         const eventTitleSlug = certificateData.event_title.replace(/[^a-zA-Z0-9]/g, '_')
         const attendeeNameSlug = certificateData.attendee_name.replace(/[^a-zA-Z0-9]/g, '_')
         const filename = `${eventTitleSlug}_${certificateData.certificate_id}.png`
-        const folderPath = `users/${userId}/certificates/${attendeeNameSlug}`
+        const folderPath = `users/${session.user.id}/certificates/${attendeeNameSlug}`
         const certificatePath = `${folderPath}/${filename}`
 
         // Save certificate to database
